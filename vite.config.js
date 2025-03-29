8e5abf45131e0d927470829c4aa5c70f71f73c84
Aryanbhargava18 <aryanbhargava644@gmail.com>
2025-03-29 20:14:59 +0530

    Portfolio updated

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    // Optionally adjust chunk size warning limit:
+    chunkSizeWarningLimit: 600,
+  },
+});
